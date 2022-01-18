317e0d11384a9ded65dbc3d00ac498f82106ee89
Miguel Chaves <mchaves.apps@gmail.com>
2022-01-18 13:30:56 +0000

    Added tests for data integrity

--- AuthorsAppTests/Data/DataTests.swift
+++ AuthorsAppTests/Data/DataTests.swift
@@ -24,4 +24,40 @@ class DataTests: XCTestCase {
 
     // MARK: - Data Tests
 
+    /// Test if the data retrivied is consistent
+    func testAuthorInformation() {
+        
+        let authorExpectation = self.expectation(description: "Test received info")
+        var authorResponse: [AuthorObject]?
+        
+        AppRequests.search(author: "tolkien") { items in
+            authorResponse = items
+            authorExpectation.fulfill()
+        }
+        
+        self.waitForExpectations(timeout: 5.0) { error in
+            
+            if let error = error {
+                XCTFail("Received info error: \(error)")
+                return
+            }
+            
+            guard let strongItems = authorResponse else {
+                XCTFail("Items are nil")
+                return
+            }
+            
+            XCTAssert(strongItems.count > 0, "Number of items is 0")
+            
+            for item in strongItems {
+                
+                guard let subjects = item.top_subjects else {
+                    XCTFail("Item: \(item.name ?? "--") without subjects")
+                    return
+                }
+                
+                print("Item: \(item.name ?? "--") with \(subjects.count) subjects")
+            }
+        }
+    }
 }
